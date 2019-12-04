0806edc85af9cd507db89b81920af8d67565302d
grvmiguel@eviala <softeviala@gmail.com>
2019-12-04 09:43:24 +0100

    Primer 42069

---
+++ 42069.js
@@ -0,0 +1,23 @@
+let net = require("net");
+let server = net.createServer();
+server.listen(42069);
+
+server.on('connection',(socket) => {
+    socket.on('data', (data) => {
+        try {
+            let json = JSON.parse(data.toString());
+            switch(json.tipo) {
+                case "REBANADADE":
+                    socket.write("GNOCILLA");
+                    break;
+                default:
+                    console.log("Default")
+                
+            }
+            console.log(json);
+        } catch (error) {
+            console.log("ERROR")
+        }
+        
+    })
+});
